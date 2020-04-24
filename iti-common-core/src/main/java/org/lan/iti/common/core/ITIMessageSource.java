3ce6ac64ead1e87aabd9a18e9cc06f7b25114446
NorthLan <lan6995@gmail.com>
2020-04-24 18:04:42 +0800

    feat: [core] 添加自定义MessageSource，读取 resources/i18n/messages 下的语言文件，做服务端多语言

---
+++ iti-common-core/src/main/java/org/lan/iti/common/core/ITIMessageSource.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Copyright (c) [2019-2020] [NorthLan](lan6995@gmail.com)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.lan.iti.common.core;
+
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * 框架定义 MessageSource
+ *
+ * @author NorthLan
+ * @date 2020-04-10
+ * @url https://noahlan.com
+ */
+public class ITIMessageSource extends ReloadableResourceBundleMessageSource {
+
+    public ITIMessageSource() {
+        setBasename("classpath:i18n/messages");
+    }
+
+    public static MessageSourceAccessor getAccessor() {
+        return new MessageSourceAccessor(new ITIMessageSource());
+    }
+}
