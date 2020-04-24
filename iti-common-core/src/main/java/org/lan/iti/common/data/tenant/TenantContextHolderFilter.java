1968c6ea6c4eb742fefc527dbb74841c56dd3f7b
NorthLan <lan6995@gmail.com>
2020-04-24 18:01:44 +0800

    fix: [core] 修复 tenantFilter 租户ID获取错误的问题

--- iti-common-core/src/main/java/org/lan/iti/common/data/tenant/TenantContextHolderFilter.java
+++ iti-common-core/src/main/java/org/lan/iti/common/data/tenant/TenantContextHolderFilter.java
@@ -16,9 +16,7 @@
 
 package org.lan.iti.common.data.tenant;
 
-import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.lan.iti.common.core.util.StringUtils;
 import org.lan.iti.common.core.constants.ITIConstants;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -43,15 +41,18 @@ public class TenantContextHolderFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String tenantId = httpServletRequest.getHeader(ITIConstants.TENANT_ID_HEADER_NAME);
+        String tenantIdStr = httpServletRequest.getHeader(ITIConstants.TENANT_ID_HEADER_NAME);
 
-        log.debug("请求Header中租户ID为：{}", tenantId);
+        log.debug("请求Header中租户ID为：{}", tenantIdStr);
 
-        if (StrUtil.isNotBlank(tenantId) || StringUtils.isNullOrUndefined(tenantId)) {
-            TenantContextHolder.setTenantId(ITIConstants.DEFAULT_TENANT_ID);
-        } else {
-            TenantContextHolder.setTenantId(Long.parseLong(tenantId));
+        Long tenantId = ITIConstants.DEFAULT_TENANT_ID;
+        try {
+            tenantId = Long.parseLong(tenantIdStr);
+        } catch (NumberFormatException e) {
+            // just catch it
         }
+        TenantContextHolder.setTenantId(tenantId);
+
         filterChain.doFilter(httpServletRequest, httpServletResponse);
         // 请求结束后清理内存
         TenantContextHolder.clear();
