9e6b4238b39b928d2db51552d689e8ec3d17dddb
NorthLan <lan6995@gmail.com>
2020-04-24 18:07:48 +0800

    feat: [core] 添加 RequestParam 时间类型 自动格式化

---
+++ iti-common-core/src/main/java/org/lan/iti/common/core/config/ITIFormatterConfig.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *  * Copyright (c) [2019-2020] [NorthLan](lan6995@gmail.com)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.lan.iti.common.core.config;
+
+import cn.hutool.core.date.DatePattern;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Web Formatter 注册器
+ *
+ * @author NorthLan
+ * @date 2020-04-02
+ * @url https://noahlan.com
+ */
+@Configuration
+@ConditionalOnClass({WebMvcConfigurer.class, DispatcherServlet.class})
+public class ITIFormatterConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        // 自定义时间转换规则
+        DateTimeFormatterRegistrar dateTimeFormatterRegistrar = new DateTimeFormatterRegistrar();
+        dateTimeFormatterRegistrar.setDateFormatter(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN));
+        dateTimeFormatterRegistrar.setTimeFormatter(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN));
+        dateTimeFormatterRegistrar.setDateTimeFormatter(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN));
+        dateTimeFormatterRegistrar.registerFormatters(registry);
+    }
+}
