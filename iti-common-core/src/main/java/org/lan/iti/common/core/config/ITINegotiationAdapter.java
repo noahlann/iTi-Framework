a08783aa90f030a41d78ddad85f7e7fd14b1c895
NorthLan <lan6995@gmail.com>
2020-04-28 22:26:48 +0800

    feat: [core] 添加默认Content-Type为application/json，避免引入xml后序列化错误

--- iti-common-core/src/main/java/org/lan/iti/common/core/config/ITINegotiationAdapter.java
+++ iti-common-core/src/main/java/org/lan/iti/common/core/config/ITINegotiationAdapter.java
@@ -1,9 +1,44 @@
+/*
+ *
+ *  * Copyright (c) [2019-2020] [NorthLan](lan6995@gmail.com)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 package org.lan.iti.common.core.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
+ * 配置默认Content-type
+ * <p>
+ * 避免引入 jackson-databind-xml 后导致的response变为xml的问题
+ *
  * @author NorthLan
  * @date 2020-04-28
  * @url https://noahlan.com
  */
-public class ITINegotiationAdapter {
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({WebMvcConfigurer.class, DispatcherServlet.class})
+public class ITINegotiationAdapter implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON, MediaType.TEXT_XML, MediaType.APPLICATION_XML);
+    }
 }
