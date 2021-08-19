3bf3136c133d41e69f5c9d52a08ab8f0d344d065
NorthLan <6995syu@163.com>
2021-08-19 20:33:10 +0800

    feat(common): 添加通用的Mapped顶层接口，替换ApiResult

---
+++ iti-common-project/iti-common-core/src/main/java/org/lan/iti/common/core/support/Mapped.java
@@ -0,0 +1,95 @@
+/*
+ *
+ *  * Copyright (c) [2019-2021] [NorthLan](lan6995@gmail.com)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.lan.iti.common.core.support;
+
+import cn.hutool.core.lang.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Mapped generic type
+ *
+ * @author NorthLan
+ * @date 2021-07-06
+ * @url https://noahlan.com
+ */
+public abstract class Mapped<Children extends Mapped<Children>> extends HashMap<String, Object> {
+    private static final long serialVersionUID = 2870321854370298093L;
+
+    public Mapped(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    public Mapped(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public Mapped() {
+        super();
+    }
+
+    public Mapped(Map<? extends String, ?> m) {
+        super(m);
+    }
+
+    @SuppressWarnings("unchecked")
+    private final Children typedThis = (Children) this;
+
+    public <T> T getByKey(String key) {
+        return getByKey(key, null);
+    }
+
+    public <T> T getByKey(String key, T defaultValue) {
+        return getByKey(key, defaultValue, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getByKey(String key, T defaultValue, Function<Object, T> cast) {
+        Assert.notEmpty(key, "key cannot be empty");
+        Object obj = this.get(key);
+        if (obj == null) {
+            return defaultValue;
+        }
+        if (cast == null) {
+            return (T) obj;
+        }
+        return cast.apply(obj);
+    }
+
+    @Override
+    public Children put(String name, Object value) {
+        Assert.notEmpty(name, "key cannot be empty");
+        super.put(name, value);
+        return typedThis;
+    }
+
+    @Override
+    @Deprecated
+    public void putAll(Map<? extends String, ?> m) {
+        throw new UnsupportedOperationException("Deprecated: method putAll(Map) in Mapped is forbidden. Use putMap(Map) instead.");
+    }
+
+    public Children putMap(Map<? extends String, ?> m) {
+        Assert.notNull(m, "map cannot be null");
+        super.putAll(m);
+        return typedThis;
+    }
+}
---
+++ iti-common-project/iti-common-core/src/main/java/org/lan/iti/common/core/util/JacksonHelper.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  * Copyright (c) [2019-2021] [NorthLan](lan6995@gmail.com)
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.lan.iti.common.core.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author NorthLan
+ * @date 2021/8/16
+ * @url https://blog.noahlan.com
+ */
+@UtilityClass
+public class JacksonHelper {
+
+    public static ObjectMapper create() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+}
